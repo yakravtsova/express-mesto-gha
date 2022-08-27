90418ce420d3cd66a3c0fbc3b16e9408b406ac86
Margarita Kravtsova <margaritaselez@yandex.ru>
2022-08-27 23:15:16 +0300

    feat: изменила обработку ошибки в модуле авторизации

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,15 +4,15 @@ const { UnauthorizedError } = require('../errors/UnauthorizedError');
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
 
-  /* if (!token) {
+  if (!token) {
     throw new UnauthorizedError('Необходима авторизация');
-  } */
+  }
 
   let payload;
   try {
     payload = jwt.verify(token, 'verysecretjwtkey');
   } catch (err) {
-    throw new UnauthorizedError('Необходима авторизация');
+    next(new UnauthorizedError('Необходима авторизация'));
   }
 
   req.user = payload;
